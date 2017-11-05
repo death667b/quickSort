c426fb9ef6c887931809bb99a73e5a4e298341aa
death667b <idaniel@spinnys.com>
2017-11-05 16:43:45 +1000

    finsihed quick_sort

--- quick_sort.py
+++ quick_sort.py
@@ -10,8 +10,10 @@ def quick_sort(arr):
         return arr
 
     pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
 
+    return quick_sort(left) + middle + quick_sort(right)
 
-    return pivot
-
-print(quick_sort([4, 2, 7, 9, 5, 1, 7]))
+print(quick_sort([4, 2, 7, 9, 5, 1, 7, 1, 43, -5, 1, 76, -3421, 5, 7]))
