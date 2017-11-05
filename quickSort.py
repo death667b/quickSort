373bb4eb01cd8e2e0e574872d358fee488e3ad07
death667b <idaniel@spinnys.com>
2017-11-05 16:22:48 +1000

    Fix linting errors

--- quickSort.py
+++ quickSort.py
@@ -1,10 +1,17 @@
-def quickSort(arr):
+"""
+Quick Sort algo in Python3
+"""
+
+def quick_sort(arr):
+    """
+    quick_sort sorts an array of integers.
+    """
     if len(arr) <= 1:
         return arr
 
     pivot = arr[len(arr) // 2]
-    
+
 
     return pivot
 
-print(quickSort([4,2,7,9,5,1,7]))
+print(quick_sort([4, 2, 7, 9, 5, 1, 7]))
