f28fa22a3b57b9586cbf1f550ab26b6a7a3aa04e
death667b <idaniel@spinnys.com>
2017-11-05 20:34:27 +1000

    Finished with basic types

--- basic_data_types.py
+++ basic_data_types.py
@@ -49,3 +49,74 @@ print(s.rjust(7))
 print(s.center(7))
 print(s.replace('l', '(ell)'))
 print('     world'.strip())
+
+print('\nContainers (Array\'s)')
+xs = [3, 1, 2]
+print(xs, xs[2])
+print(xs[-1])
+xs[2] = 'foo'
+print(xs)
+xs.append('bar')
+print(xs)
+x = xs.pop()
+print(x, xs)
+
+print('\nSlicing with List\'s')
+num = list(range(5))
+print(num)
+print(num[2:4])
+print(num[2:])
+print(num[:2])
+print(num[:-1])
+num[2:4] = [8, 9]
+print(num)
+
+print('\nLoops')
+animals = ['cats', 'dogs', 'monkey']
+for idx, animal in enumerate(animals):
+    print('#%d: %s' % (idx + 1, animal))
+
+print('\nList - Long way')
+nums = list(range(5))
+squares = []
+for x in nums:
+    squares.append(x ** 2)
+print(squares)
+
+print('\nList - comprehension')
+nums = list(range(5))
+squares = [x ** 2 for x in nums]
+print(squares)
+
+print('\nList - comprehension with conditionals')
+nums = list(range(5))
+squares = [x ** 2 for x in nums if x % 2 == 0]
+print(squares)
+
+print('Dictionaries')
+d = {'cat': 'cute', 'dog': 'furry'}
+print(d['cat'])
+print('cat' in d)
+d['fish'] = 'wet'
+print(d['fish'])
+# print(d['monkey']) <- blows up!
+print(d.get('monkey','Monkey not found'))
+print(d.get('fish', 'Fish not found'))
+del d['fish']
+print(d.get('fish', 'Fish not found'))
+
+print('\nLast - Classes')
+class Greeter(object):
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self, loud=False):
+        if loud:
+            print('HELLO %s' % (self.name.upper()))
+        else:
+            print('Hello %s' % (self.name))
+
+g = Greeter('world')
+g.greet()
+g.greet(True)
