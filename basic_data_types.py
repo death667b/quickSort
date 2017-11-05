33e9796d4fb9d6f580f43067466ff33537022075
death667b <idaniel@spinnys.com>
2017-11-05 16:57:57 +1000

    First run of basic data types

---
+++ basic_data_types.py
@@ -0,0 +1,21 @@
+"""
+Some basic data types in python3
+"""
+x = 3
+print(type(x), ' = <class \'int\'>')
+print(x, ' = 3')
+print(x + 1, ' = 4')
+print(x - 1, ' = 2')
+print(x * 2, ' = 6')
+print(x ** 2, ' = 9')
+
+x += 1
+print(x, ' = 4')
+
+x *= 2
+print(x, ' = 8')
+
+y = 2.5
+print(type(y), ' = <class \'float\'>')
+print(y, y + 1, y * 2, y ** 2)
+print('2.5 3.5 5.0 6.25')
